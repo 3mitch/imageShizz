ccec8b0d128a2baf668f25722dcce69e72f844f9
Paul Treszczotko <paulcfx@gmail.com>
2011-05-12 22:25:30 -0400

    more chanegs

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
+/*
+includes
+*/
 var express = require('express');
 var app = express.createServer();
-
-
+/*
+config settings
+*/
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(app.router);
 });
-
+/*
+routes 
+*/
 app.get('/', function(req, res) {
 	res.render('index', { title: 'Image shizz!'});
 });
 
 app.get('/resize', function(req, res) {
-	res.render('index', { title: 'Resize my shizzIMG!'});
+	res.render('resize', { title: 'Resize my shizzIMG!'});
 });
 
 app.listen(3000);
